0299d523341b3655bd56d4dae9c32dc96619515c
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-09-22 12:27:11 +0900

    anki word adder

--- anki_word_adder.py
+++ anki_word_adder.py
@@ -1,3 +1,54 @@
+import requests
+import os
 
-def add_word_to_anki_deck(audio_file, translation):
-  print(audio_file + " added to anki deck (NOT IMPLEMENTED)")
+#could move this into env vars
+deck_name = 'jp_audio_cards'
+anki_connect_url = 'http://localhost:8765'
+
+def get_card_options(deck_name):
+  options = {
+    "allowDuplicate": False,
+    "duplicateScope": "deck",
+    "duplicateScopeOptions": {
+        "deckName": deck_name,
+        "checkChildren": False,
+        "checkAllModels": False
+    }
+  },
+  return options
+  
+def create_anki_audio(audio_file_path):
+  audio_absolute_path = os.path.abspath(audio_file_path)
+  audio_base_name = os.path.basename(audio_file_path)
+  audio = [{
+    "path": audio_absolute_path,
+    "filename": audio_base_name,
+    "skipHash": "7e2c2f954ef6051373ba916f000168dc",
+    "fields": ["Front"]
+  }]
+  return audio
+
+def create_audio_card(deck_name, front, back, audio_file_path):
+  card = {
+    "action": "addNote",
+    "version": 6,
+    "params": {
+        "note": {
+            "deckName": deck_name,
+            "modelName": "Basic",
+            "fields": {
+                "Front": front,
+                "Back": back
+            },
+            "options": get_card_options(deck_name),
+            "audio": create_anki_audio(audio_file_path),
+        }
+    }
+  }
+  return card
+
+def add_card_to_anki_deck(audio_file, translation):
+  card = create_audio_card(deck_name, "", translation, audio_file)
+  response = requests.post(anki_connect_url, json=card)
+  print(response.json())
+  return response.json()
