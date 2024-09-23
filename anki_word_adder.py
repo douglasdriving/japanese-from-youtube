d2b4d4de9ff72e97e99e90d24cb95fd1f7d3061d
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-09-23 18:28:22 +0900

    auto anki open

--- anki_word_adder.py
+++ anki_word_adder.py
@@ -1,9 +1,30 @@
 import requests
 import os
+import subprocess
+import time
 
 #could move this into env vars
 deck_name = 'jp_audio_cards'
 anki_connect_url = 'http://localhost:8765'
+anki_path = r"C:\Users\dougl\AppData\Local\Programs\Anki\anki.exe"
+
+def is_anki_running():
+    try:
+        response = requests.get("http://localhost:8765")
+        return response.status_code == 200
+    except requests.exceptions.ConnectionError:
+        return False
+      
+def open_anki():
+    try:
+        subprocess.Popen([anki_path])
+        print("Opening Anki...")
+    except Exception as e:
+        print(f"Failed to open Anki: {e}")
+        
+def open_anki_if_not_running():
+    if not is_anki_running():
+        open_anki()
 
 def get_card_options(deck_name):
   options = {
@@ -48,6 +69,11 @@ def create_audio_card(deck_name, front, back, audio_file_path):
   return card
 
 def add_card_to_anki_deck(audio_file, translation):
+  open_anki_if_not_running()
+  time.sleep(3) #just to make sure it gets time to open. not ideal but works for now
   card = create_audio_card(deck_name, "", translation, audio_file)
   response = requests.post(anki_connect_url, json=card)
+  print(response.json())
   return response.json()
+
+add_card_to_anki_deck("./audios/arubamy.wav", "album")
--- youtubeWordExtractor.py
+++ youtubeWordExtractor.py
@@ -6,11 +6,15 @@ def extract_unique_words_from_youtube(video_id):
   japanese_transcript = get_japanese_transcript(video_id)
   
   if japanese_transcript is not None:
-    print("Successfully retrieved Japanese transcript.")
+    line_count = len(japanese_transcript)
+    print("Successfully retrieved Japanese transcript. Lines to extract words from: " + str(line_count))
     allWords = []
+    lines_extracted_from = 1
     for line in japanese_transcript:
+      print("extracting line " + str(lines_extracted_from) + "...")
       allWords += extract_japanese_words(line['text'])
-      print("Extracted words from line: " + line['text'])
+      print("Extracted words from line " + str(lines_extracted_from) + "/" + str(line_count) + ": " + line['text'])
+      lines_extracted_from += 1
     uniqueWords = list(set(allWords))
     return uniqueWords
   else:
