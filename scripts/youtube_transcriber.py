44aea93303de5db41043e306965d6845cad2909a
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-10-25 10:45:37 +0900

    transcribes mixed languages

--- scripts/youtube_transcriber.py
+++ scripts/youtube_transcriber.py
@@ -17,20 +17,13 @@ def combine_transcript_into_single_text(transcript):
     return transcript_as_single_string
 
 
-def get_japanese_transcript(video_id):
+def get_transcript(video_id):
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=["ja"])
-        return transcript
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
-
-def get_youtube_transcript(video_id):
-    transcript = get_japanese_transcript(video_id)
-    if transcript is not None:
+        transcript = YouTubeTranscriptApi.get_transcript(
+            video_id, languages=["en", "ja"]
+        )
         transcript_as_single_text = combine_transcript_into_single_text(transcript)
         return transcript_as_single_text
-    else:
-        raise Exception("Failed to get transcript")
+    except Exception as e:
+        print(f"Failed to get transcript. Error: {e}")
         return None
