f258dc6f99c74cfd1e286179d88bb4b4442aecf9
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-11-03 08:10:41 +0900

    cleanup

--- scripts/audio/audio_player.py
+++ scripts/audio/audio_player.py
@@ -1,27 +1,24 @@
 from pydub import AudioSegment
 from pydub.playback import play
 
+
 class AudioPlayer:
-  def __init__(self, audio_file = ""):
-    self.audio_file = audio_file
-    self.sound = None
+    def __init__(self, audio_file=""):
+        self.audio_file = audio_file
+        self.sound = None
 
-  def load_audio(self):
-    self.sound = AudioSegment.from_file(self.audio_file, format="wav")
-    
-  def set_audio_file(self, audio_file):
-    self.audio_file = audio_file
-    self.sound = None
+    def load_audio(self):
+        self.sound = AudioSegment.from_file(self.audio_file, format="wav")
 
-  def play_audio(self):
-    if self.sound is None:
-      self.load_audio()
-    play(self.sound)
-    
-  def play_audio_file(self, audio_file):
-    self.set_audio_file(audio_file)
-    self.play_audio()
+    def set_audio_file(self, audio_file):
+        self.audio_file = audio_file
+        self.sound = None
 
-# Example usage:
-# player = AudioPlayer()
-# player.play_audio_file('./audios/arubamu.wav')
+    def play_audio(self):
+        if self.sound is None:
+            self.load_audio()
+        play(self.sound)
+
+    def play_audio_file(self, audio_file):
+        self.set_audio_file(audio_file)
+        self.play_audio()
