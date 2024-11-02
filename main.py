fa083e6ddcaf5c07b6e39389eae207f203f52f52
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-11-03 07:54:09 +0900

    turned main into class

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from scripts.youtube_scraper import YoutubeScraper
-
 from scripts.data_cleaner.data_cleaner import DataCleaner
 
-cleaner = DataCleaner()
-cleaner.clean_data()
-youtube_scraper = YoutubeScraper()
-youtube_scraper.scrape_video()
+
+class MainProgram:
+
+    data_cleaner: DataCleaner
+    youtube_scraper: YoutubeScraper
+
+    def __init__(self):
+        self.data_cleaner = DataCleaner()
+        self.youtube_scraper = YoutubeScraper()
+
+    def run(self):
+        self.data_cleaner.clean_data()
+        self.youtube_scraper.scrape_video()
+        input("Program finished. Press Enter to exit...")
 
 
-# from scripts.youtube_transcriber import get_transcript
-
-# transcript = get_transcript("QnLABAkqxNs")
-# for line in transcript:
-#     print(line.text)
-
-
-input("Program finished. Press Enter to exit...")
+main_program = MainProgram()
+main_program.run()
