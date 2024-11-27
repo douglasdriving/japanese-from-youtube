67da19fca6f2cc08a131094e27ece9b1fb818479
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2024-11-28 08:21:49 +0900

    words interval 4 required

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ import dotenv
 
 class MainProgram:
 
-    data_cleaner: DataCleaner
-    youtube_scraper: YoutubeScraper
-    progress_detector: ProgressDetector
+    data_cleaner = DataCleaner()
+    youtube_scraper = YoutubeScraper()
+    progress_detector = ProgressDetector()
 
     def __init__(self):
         dotenv.load_dotenv()
         input("Please open Anki! Then press enter to continue...")
-        self.data_cleaner = DataCleaner()
-        self.youtube_scraper = YoutubeScraper()
-        self.progress_detector = ProgressDetector()
 
     def run(self):
         self.data_cleaner.clean_data()
